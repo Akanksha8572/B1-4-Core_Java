f2da7dbba69ea3eafefe5c6bc83753d16d534437
HP <HP@DESKTOP-JHVNQ96>
2022-04-03 13:46:34 -0700

    First commit

---
+++ Day.1/src/com/tns/git/Example1.java
@@ -0,0 +1,13 @@
+package com.tns.git;
+
+public class Example1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Akanksha Singh");
+		System.out.print("Ankita Rai");
+	
+
+	}
+
+}
