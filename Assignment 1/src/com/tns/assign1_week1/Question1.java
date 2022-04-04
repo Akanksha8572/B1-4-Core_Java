1b031f1c9c3533ea156073e89e6431b7b18c0798
HP <HP@DESKTOP-JHVNQ96>
2022-04-04 12:15:23 -0700

    First Commit

---
+++ Assignment 1/src/com/tns/assign1_week1/Question1.java
@@ -0,0 +1,18 @@
+package com.tns.assign1_week1;
+
+import java.util.Scanner;
+public class Question1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+				int a = sc.nextInt();
+		do {
+			
+				System.out.println(++a);
+			
+		}while (a<=50);
+		
+		sc.close();
+	}
+
+}
---
+++ Assignment 1/src/com/tns/assign1_week1/Question2.java
@@ -0,0 +1,21 @@
+package com.tns.assign1_week1;
+
+public class Question2 {
+
+	public static void main(String args[]) {
+	      int [] numbers = {20, 30, 40, 50, 60};
+
+	      for(int x : numbers ) {
+	         System.out.print( x );
+	         System.out.print(",");
+	      }
+	      System.out.print("\n");
+	      String [] names = {"Janny", "Piya", "Thom", "Lam"};
+
+	      for( String name : names ) {
+	         System.out.print( name );
+	         System.out.print(",");
+	      }
+	   }
+
+}
