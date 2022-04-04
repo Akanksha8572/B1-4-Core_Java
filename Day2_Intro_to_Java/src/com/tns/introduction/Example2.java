2865a029ec849be4fc04a29ee23322f61eb035b1
HP <HP@DESKTOP-JHVNQ96>
2022-04-04 00:15:41 -0700

    First Commit

---
+++ Day2_Intro_to_Java/src/com/tns/introduction/Example2.java
@@ -0,0 +1,19 @@
+package com.tns.introduction;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		float y=s.nextFloat();
+		System.out.println(x);
+		System.out.print(y);
+		s.close();
+		
+
+	}
+
+}
