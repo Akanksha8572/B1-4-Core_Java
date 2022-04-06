22642481579933a205e17819dcc6c9847c9dd2d5
HP <HP@DESKTOP-JHVNQ96>
2022-04-05 19:17:47 -0700

    First Commit

---
+++ Day7_This_static/src/com/tns/static1/Example2.java
@@ -0,0 +1,16 @@
+package com.tns.static1;
+
+public class Example2
+{
+	static void print()
+	{
+		System.out.println("Nikki is doing well in the class");
+	}
+
+	public static void main(String[] args) 
+	{
+		print();
+
+	}
+
+}
