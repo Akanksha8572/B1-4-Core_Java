f0a265fb92e01cd36c4572ede7fd0205ca9f63eb
HP <HP@DESKTOP-JHVNQ96>
2022-05-04 17:16:19 -0700

    First Commit

---
+++ Day21_Annotation/src/com/tns/annotation/Example3.java
@@ -0,0 +1,25 @@
+package com.tns.annotation;
+
+class Parent1
+{
+	@Deprecated
+	void print()
+	{
+		System.out.println("Hey guys");
+	}
+	@Deprecated
+	void print1()
+	{
+		System.out.println("hurry up");
+	}
+}
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+		Parent1 p=new Parent1();
+		p.print();
+		p.print1();
+	}
+
+}
